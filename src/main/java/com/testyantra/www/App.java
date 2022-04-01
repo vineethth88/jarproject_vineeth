0a7fba61e49af7d258513ed0259c976c04788214
vineeth <vineeth_t@cloudbots.com>
2022-04-01 12:23:23 +0000

    adding the .jar files into the repo

---
+++ src/main/java/com/testyantra/www/App.java
@@ -0,0 +1,15 @@
+package com.testyantra.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! From TestYantra Project" );
+        System.out.println( "This im trying to do this for Devops practise and give interveiw" );
+    }
+
+}
